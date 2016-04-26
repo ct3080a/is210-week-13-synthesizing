5e2a5b05a202190f5f7fd517d9bdffc297d69a83
paisely65 <cunysps@gmail.com>
2016-04-26 19:06:00 -0400

    Committask1.

--- picklecache.py
+++ picklecache.py
@@ -8,27 +8,33 @@ import pickle
 
 class PickleCache(object):
     """This class inherits from object."""
-    if __name__ == "__main__":
-            '_PickleCache__file_path' == "file_path"
-            '_PickleCache__data' == {}
-           
-
-def __init__(self, file_path='datastore.pkl', autosync=False):
+    
+    def __init__(self, file_path='datastore.pkl', autosync=False):
         """This constructor function has two arguments.
+
         Args:
-            file_path (string, optional): Defaults to datastore.pkl
-            autosync (bool, optional): Defaults to False
+        file_path (string, optional): Defaults to datastore.pkl
+        autosync (bool, optional): Defaults to False
 
         Attributes:
-            autosync(bool): creates an auto sync
+        autosync(bool): defaults to False
+        __file__path(string): creates a string object.
+        __data(dictionary): a set of empty key value pairs.
 
-           __file__path(string): creates a string object
+        Returns:
+            None
 
-           __data(dictionary): a set of key value pairs
+        Example:
+            >>> cacher = PickleCache()
+            >>> kprint cacher._PickleCache__file_path
+           'datastore.pkl'
+            >>> print cacher._PickleCache__file_object
+            None
+            >>> print cacher._PickleCache__data
+            {}
         """
         self.__file_path = file_path
         self.__data = {}
+        self.__file_object = None
         self.load()
-        autosync = self.autosync
-
-        
+        self.autosync = self.autosync
