3c025269c827aee9d1ea6fd2e21aa8d3e14542d1
paisely65 <cunysps@gmail.com>
2016-04-26 20:04:38 -0400

    Task one revised.

--- picklecache.py
+++ picklecache.py
@@ -38,3 +38,141 @@ class PickleCache(object):
         self.__file_object = None
         self.load()
         self.autosync = self.autosync
+
+    def __setitem__(self, key, values):
+        """A functions that converts the cache to a dictionary.
+
+        Arguments:
+        Key (strings): Keys to the ditionary _data.
+        value (mix): Values for the data dictionary.
+
+        Return:
+            None
+
+        Example:
+            >>> pcache = PickleCache()
+            >>> pcache['test'] = 'hello'
+            >>> print pcache._PickleCache__data['test']
+            'hello'
+            >>> len(pcache)
+            1
+        """
+
+        try:
+            self.__data[key] = values
+            if self.autosync is True:
+
+                self.flush()
+        except (KeyError, TypeError) as error:
+            raise error
+
+        def __len__(self):
+            """A function that controls the length of a dictionary
+
+            Returns:
+                The length of self.__data
+
+            Example:
+                >>> pcache = PickleCache()
+                >>> pcache['test'] = 'Hello World'
+                >>> len(pcache)
+                2
+            """
+
+            return len(self.__data)
+
+    def __getitem__(self, key):
+        """A Function that gets items in the __data dictionary.
+
+        Return:
+            The value in self.__data dictionary.
+
+        Example:
+            >>> pcache = PickleCache()
+            >>> pcache['test'] = 'Hello World'
+            >>> print pcachel['test']
+            'Hello World'
+        """
+
+        try:
+            if self.__data[key]:
+                datakey = self.__data[key]
+                if self.autosync is True:
+                    self.flush()
+            return datakey
+
+        except (TypeError, KeyError) as error:
+            raise error
+
+    def __delitem__(self, key):
+        """A function that edits the entries from the __data dict.
+
+        Arguments:
+            key (mix): Position of entries to be deleted from __data dict.
+
+        Return:
+            none
+
+        Examples:
+            >>> pcache = PickleCache()
+            >>> pcache['test'] = 'hello'
+            >>> print len(pcache)
+            1
+            >>> del pcache['test']
+            >>> print len(pcache)
+            0
+       """
+
+        if self.__data[key]:
+            del self.__data[key]
+            if self.autosync is True:
+                self.flush()
+    
+    def load(self):
+        """ A function that opens and reads a file.
+
+        Returns:
+            None
+
+        Examples:
+            >>> import pickle
+            >>> filehandler = open('datastore.pkl', 'w')
+            >>> pickle.dump({'foo': 'bar'}, filehandler)
+            >>> filehandler.close()
+            >>> pcache = PickleCache('datastore.pkl')
+            >>> print pcache['foo']
+            'bar'
+        """
+
+        loadfile = self.__file_path
+        if os.path.exists(loadfile) and os.path.getsize(loadfile) > 0:
+            filedata = open(self.__file_path, 'r')
+            self.__data = pickle.load(filedata)
+            filedata.close()
+
+    def flush(self):
+        """A function that stores data in PickleCache__data dict>.
+
+        Arguments:
+            filefdata (file): File to be written to.
+
+        Returns:
+            None
+
+        Examples:
+            >>> pcache = PickleCache()
+            >>> pcache['foo'] = 'bar'
+            >>> pcache.flush()
+            >>> fhanfler = open(pcache.PickleCache__file_path, 'r')
+            >>> data = pickle.load(fhandler)
+            >>> print data
+            {'foo': 'bar'}
+        """
+
+        filedata = open(self.__file_path, 'w')
+        self.__file_object = filedata
+        pickle.dump(self.__data, self.__file_object)
+        filedata.close()           
+
+            
+            
