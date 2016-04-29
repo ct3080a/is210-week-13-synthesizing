498422ca4976c0084d43e7815fec6542a0d0457d
paisely65 <cunysps@gmail.com>
2016-04-28 21:05:46 -0400

    Task 1.

--- picklecache.py
+++ picklecache.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""This module uses I/O functions."""
+"""Week 13 Docstring"""
+
 import os
 import pickle
 
-
 class PickleCache(object):
-    """A class with two args."""
+    """A picklecache class."""
+
     def __init__(self, file_path='datastore.pkl', autosync=False):
-        """Constructor for PickleCache class
+        """A constructor for the file.
+
         Args:
-             file_path (string, optional) Defaults to datastore.pkl
-             autosync (bool, optional) Defaults to False
+            file_path(string, optional) Defaults to datastore.pkl
+            autosync (bool, optional) Defaults to False
+
         Attributes:
-             file_path (string, optional) Defaults to datastore.pkl
-             autosync (bool, optional) Defaults to False
-             data (dict)
+            file_path (string, optional) Defaults to datastore.pkl
+            autosync (bool, optional) Defaults to datastore.pkl
+            data (dict)
         """
         self.__file_path = file_path
         self.autosync = autosync
@@ -23,44 +26,49 @@ class PickleCache(object):
         self.load()
 
     def __setitem__(self, key, value):
-        """Creates storage for key and value pairs in dictionary. 
+        """Creates storage for key, value pairs
+
         Args:
-            key (required)
-            value (required)
-         """
+            key (mixed)
+            Value (mixed)
+        """
         self.__data[key] = value
         if self.autosync is True:
             self.flush()
 
     def __len__(self):
-        """Returns self._data length."""
+        """Returns length."""
         return len(self.__data)
 
     def __getitem__(self, key):
-        """Retreives data from the class object.
+        """Retrieves data.
+
         Args:
-             key (required)
+            key
         """
         return self.__data[key]
-    
+
     def __delitem__(self, key):
-        """Deletes data from class object.
+        """Deletes items
+
         Args:
-            key (required)       """
+            key
+        """
         del self.__data[key]
         if self.autosync is True:
             self.flush()
 
     def load(self):
-        """Opens and reads files."""
+        """Iterates through pickle files."""
         if os.path.exists(self.__file_path) is True and \
-                          os.path.getsize (self.__file_path) > 0:
-            filehandler = open(self.__file_path, 'r')
-            self.__data = pickle.load(filehandler)
-        filehandler.close()
+           os.path.getsize(self.__file_path) > 0:
+            with open(self.__file_path, 'r') as fhd:
+                self.__data = pickle.load(fhd)
+                fhd.close()
 
     def flush(self):
-        """Stores data."""
-        filehandler = open(self.__file_path, 'w')
-        pickle.dump(self.__data, filehandler)
-        filehandler.close()
+        """Saves and manages files."""
+        with open(self.__file_path, 'w') as fhd:
+            pickle.dump(self.__data. fhd)
+        fhd.close()
+             
